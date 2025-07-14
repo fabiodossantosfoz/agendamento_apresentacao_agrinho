e076fd403f9249ed2f53fcc92c3cba5c0fb7870d
fabiodossantosfoz <fabio_santos@escola.pr.gov.br>
2025-07-14 11:59:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,12 @@ CORS(app)
 app.register_blueprint(user_bp, url_prefix='/api')
 app.register_blueprint(agendamento_bp, url_prefix='/api')
 
-# uncomment if you need to use database
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.path.dirname(__file__), 'database', 'app.db')}"
+# Configuração do Banco de Dados PostgreSQL a partir da variável de ambiente
+db_url = os.environ.get('DATABASE_URL')
+if db_url and db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 with app.app_context():
