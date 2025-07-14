3fe616d56dee266e77b510fd531f5f3d1edfe97f
fabiodossantosfoz <fabio_santos@escola.pr.gov.br>
2025-07-14 14:43:27 -0300

    Update user.py

--- src/routes/user.py
+++ src/routes/user.py
@@ -1,1 +1,10 @@
+from flask import Blueprint
 
+# Este é o blueprint para as rotas de usuário.
+# Mesmo que não tenhamos nenhuma rota aqui ainda, o objeto precisa existir para ser importado.
+user_bp = Blueprint('user_bp', __name__)
+
+# Aqui você poderia adicionar rotas no futuro, como:
+# @user_bp.route('/users', methods=['GET'])
+# def get_users():
+#     return "Lista de usuários"
